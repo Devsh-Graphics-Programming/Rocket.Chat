8c74d156dea04f850c07ed2391db10e7cffedb82
Nazareno Bucciarelli <bnazareno03@gmail.com>
2026-05-04 18:50:39 -0300

    add server-side guard to avoid message parser when setting value is exceeded

--- apps/meteor/server/services/messages/hooks/BeforeSaveMarkdownParser.ts
+++ apps/meteor/server/services/messages/hooks/BeforeSaveMarkdownParser.ts
@@ -2,6 +2,8 @@ import { isE2EEMessage } from '@rocket.chat/core-typings';
 import type { IMessage } from '@rocket.chat/core-typings';
 import { parse } from '@rocket.chat/message-parser';
 
+import { settings } from '../../../../app/settings/server';
+
 type ParserConfig = {
 	colors?: boolean;
 	emoticons?: boolean;
@@ -23,6 +25,11 @@ export class BeforeSaveMarkdownParser {
 		}
 
 		if (isE2EEMessage(message)) {
+			return message;
+		}
+
+		const maxParseLength = settings.get<number>('Message_MaxMarkdownParseLength');
+		if (maxParseLength > 0 && message.msg && message.msg.length > maxParseLength) {
 			return message;
 		}
 
